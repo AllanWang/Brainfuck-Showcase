9e0058b890c00f41eaae2695d21079dc5f9ccd13
AllanWang <me@allanwang.ca>
2016-11-27 03:20:46 -0500

    hello world

--- src/brainfuck/Compiler.java
+++ src/brainfuck/Compiler.java
@@ -4,14 +4,24 @@ import java.util.Stack;
 
 /**
  * Created by Allan Wang on 2016-11-27.
- *
+ * <p>
  * Original code from https://github.com/quwahara/Brainfuck
+ * <p>
+ * Brainfuck is a "simple" programming language with only 8 characters
+ * <>+-[],.
+ * See the comments below for their uses
  */
 public class Compiler {
+
     public static void brainfuck(String bf) throws Exception {
+        brainfuck(bf, false);
+    }
+
+    public static void brainfuck(String bf, boolean numbers) throws Exception {
         int[] loop = new int[bf.length()];
         Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < bf.length(); ++i) {
+        //Get all the while loops and their respective locations
+        for (int i = 0; i < bf.length(); i++) {
             switch (bf.charAt(i)) {
                 case '[':
                     stack.push(i);
@@ -26,15 +36,16 @@ public class Compiler {
             }
         }
 
-        int pc = 0, ptr = 0;
+        int pc = 0, ptr = 20; //default pointer to 20 to allow for "negative" pointer indices
         byte[] mem = new byte[30000];
+        //Start reading Brainf*ck code
         while (pc < bf.length()) {
             switch (bf.charAt(pc)) {
                 case '<':
                     ptr--; //move pointer back one
                     break;
                 case '>':
-                    ptr++;//move pointer forward one
+                    ptr++; //move pointer forward one
                     break;
                 case '-':
                     mem[ptr]--; //remove one from current pointer
@@ -49,7 +60,8 @@ public class Compiler {
                     if (mem[ptr] != 0) pc = loop[pc] - 1; //if pointer is not 0, move back to start position
                     break;
                 case '.':
-                    System.out.print((char) mem[ptr]); //print out current character
+                    if (!numbers) System.out.print((char) mem[ptr]); //print out current character
+                    else System.out.print((int) mem[ptr] + " ");
                     break;
                 case ',':
                     mem[ptr] = (byte) System.in.read(); //read one byte
--- src/brainfuck/Showcase.java
+++ src/brainfuck/Showcase.java
@@ -6,11 +6,16 @@ package brainfuck;
  */
 public class Showcase {
 
-    public static String echo = ",[.[-]>,]"; //echos whatever you print
+    public static String
+            echo = ",[.[-]>,]", //echos whatever you print
 
-    private static String toCompile = echo;
+    //prints Hello World; for more samples in many other languages, see
+    //http://codegolf.stackexchange.com/questions/55422/hello-world
+    bestHelloWorld = "--<-<<+[+[<+>--->->->-<<<]>]<<--.<++++++.<<-..<<.<+.>>.>>.<<<.+++.>>.>>-.<<<+.", //by primo
+
+    toCompile = bestHelloWorld;
 
     public static void main(String[] args) throws Exception {
-        Compiler.brainfuck(toCompile);
+        Compiler.brainfuck(toCompile, true);
     }
 }
